d1fb58d9cc24fed79c0707dbacfc29bdbead1d4f
Rodrigo Sim <rcsim10@gmail.com>
2023-09-12 22:23:12 -0300

    Initial Spring Boot Application for Parking Meter Project

---
+++ src/main/java/com/postech30/parkingmeter/ParkingmeterApplication.java
@@ -0,0 +1,13 @@
+package com.postech30.parkingmeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingmeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingmeterApplication.class, args);
+	}
+
+}
