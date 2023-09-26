1734d742a1c7b20adf64a850c3af0d87f7f9b912
Geovane Silveira <geovanedsilveira@gmail.com>
2023-09-26 01:44:11 -0300

    add class UserRepository
    Faltando implementar

---
+++ src/main/java/com/postech30/parkingmeter/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.postech30.parkingmeter.repository;
+
+import com.postech30.parkingmeter.entity.User;
+import com.postech30.parkingmeter.entity.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    
+}
