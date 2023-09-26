1b5214eea68eda8a37fc6c04e80565713636c2ca
Geovane Silveira <geovanedsilveira@gmail.com>
2023-09-26 01:37:27 -0300

    add class UserDTO
    Faltando implementar

---
+++ src/main/java/com/postech30/parkingmeter/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package com.postech30.parkingmeter.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    
+}
