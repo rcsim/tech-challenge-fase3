d93471420673da6b37933488236786e56fa11d01
Geovane Silveira <geovanedsilveira@gmail.com>
2023-09-26 01:55:56 -0300

    add class UserService
    Faltando implementar

---
+++ src/main/java/com/postech30/parkingmeter/service/impl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.postech30.parkingmeter.service.impl;
+
+import com.postech30.parkingmeter.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    
+}
