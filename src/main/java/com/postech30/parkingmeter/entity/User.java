bf055f8d7d6f8a9ddc4a96c4bd3cf392509e64ed
Geovane Silveira <geovanedsilveira@gmail.com>
2023-10-06 11:30:06 -0300

    Refatored entity user

--- src/main/java/com/postech30/parkingmeter/entity/User.java
+++ src/main/java/com/postech30/parkingmeter/entity/User.java
@@ -2,20 +2,13 @@ package com.postech30.parkingmeter.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.DiscriminatorOptions;
-import org.hibernate.validator.constraints.br.CNPJ;
-import org.hibernate.validator.constraints.br.CPF;
 
 import java.io.Serializable;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-//@Table(name = "tb_users")
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "personType", discriminatorType = DiscriminatorType.STRING)
 public class User implements Serializable {
 
     @Id
@@ -27,9 +20,5 @@ public class User implements Serializable {
     private String email;
 
     private String telephone;
-
-    @ManyToOne
-    @JoinColumn(name = "address_id")
-    private Address address;
 
 }
