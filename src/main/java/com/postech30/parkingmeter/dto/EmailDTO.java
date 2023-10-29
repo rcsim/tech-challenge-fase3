15afc8bde254fe7ccc24c0a0f7c7b39ea0b19eea
Marcos Fernandes Alves <marcosfernandesalves99@hotmail.com>
2023-10-29 11:27:15 -0300

    fazendo alteração email

---
+++ src/main/java/com/postech30/parkingmeter/dto/EmailDTO.java
@@ -0,0 +1,17 @@
+package com.postech30.parkingmeter.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailDTO {
+
+    private String emailFrom;
+    private String emailToto;
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,7 +2,7 @@ INSERT INTO tb_vehicles(manufacturer, model, plate) VALUES ('Ferrari', 'Ferrari 
 
 INSERT INTO tb_tickets(vehicle_id, check_in, check_out, status, price) VALUES (1, '2023-09-24T10:10:01.176581600Z', '2023-09-24T23:10:01.176581600Z', 'closed', 91.00);
 INSERT INTO tb_tickets(vehicle_id, check_in, check_out, status, price) VALUES (1, '2023-09-24T10:10:01.176581600Z', null, 'open', 0.00);
-INSERT INTO tb_tickets(vehicle_id, check_in, check_out, status, price) VALUES (1, '2023-09-24T10:10:01.176581600Z', '2023-09-24T12:10:01.176581600Z', 'closed', 14.00);
+INSERT INTO tb_tickets(vehicle_id, check_in, check_out, status, price) VALUES (1, '2023-09-24T10:10:01.176581600Z', '2023-09-24T12:10:01.176581600Z', 'closed', 1.00);
 INSERT INTO tb_tickets(vehicle_id, check_in, check_out, status, price) VALUES (1, '2023-09-24T17:17:01.176581600Z', '2023-09-24T23:10:01.176581600Z', 'closed', 42.00);
 INSERT INTO tb_tickets(vehicle_id, check_in, check_out, status, price) VALUES (1, '2023-09-24T17:10:01.176581600Z', null, 'open', 0.00);
 
