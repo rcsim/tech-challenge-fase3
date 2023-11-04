d58d55f7e1fb2dc75aee3fc7df3abb0d526b5d10
Victor Toledo <victorptoledo@outlook.com>
2023-11-03 22:38:37 -0300

    Incluindo a documentação do Swagger do cartão e do usuário, e incluindo a obrigatoriedade dos campos nome e e-mail no cadastro do usuario.

--- src/main/java/com/postech30/parkingmeter/dto/UserDTO.java
+++ src/main/java/com/postech30/parkingmeter/dto/UserDTO.java
@@ -1,6 +1,8 @@
 package com.postech30.parkingmeter.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.postech30.parkingmeter.entity.User;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Data
@@ -9,10 +11,16 @@ import lombok.*;
 public class UserDTO {
 
     private Long id;
-    private String name;
-    private String email;
-    private String telephone;
 
+    @JsonProperty
+    @NotNull(message = "O nome é um campo de preenchimento obrigatório")
+    private String name;
+
+    @JsonProperty
+    @NotNull(message = "O e-mail é um campo de preenchimento obrigatório")
+    private String email;
+
+    private String telephone;
 
     public UserDTO(User entity) {
         id = entity.getId();
