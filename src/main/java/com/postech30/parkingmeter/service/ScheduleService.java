cfb4ba8eacae5bacc1134b9056e10da59d93caa5
Marcos Fernandes Alves <marcosfernandesalves99@hotmail.com>
2023-11-05 16:52:44 -0300

    alterando textos emails

--- src/main/java/com/postech30/parkingmeter/service/ScheduleService.java
+++ src/main/java/com/postech30/parkingmeter/service/ScheduleService.java
@@ -1,6 +1,8 @@
 package com.postech30.parkingmeter.service;
 
+import jakarta.mail.MessagingException;
+
 public interface ScheduleService {
 
-    public void sendNotification();
+    public void sendNotification() throws MessagingException;
 }
